d16df2c40999333486923e3ac788abd2306f1585
jaehyuk <jaehyuk.pyon@gmail.com>
2023-02-09 14:54:57 +0900

    4. 삭제기능 구현

--- my-todo1/src/components/TodoMain.vue
+++ my-todo1/src/components/TodoMain.vue
@@ -5,34 +5,26 @@
     <main>
       <div class="todos">
 
-        <div class="write">
+        <div v-if="writeState === 'add'" class="write"> <!--새로운 걸 등록시-->
           <input type="text" v-model="addItemText" @keyup.enter="addItem" ref="writeArea" />
           <button class="btn add" @click="addItem">Add to List</button>
         </div>
+        <div v-else class="write"> <!--기존 걸 수정시-->
+          <input type="text" v-model="editItemText" @keyup.enter="editSave" />
+          <button class="btn add" @click="editSave">Save (Edit)</button>
+        </div>
 
         <ul class="list">
-          <!-- <li>
-            한 줄로 렌더링됨
-            <i class="far fa-check-square"></i>
-            >>> fas: checked, far: unchecked
-            <span>
-              원고 챕터1 마감
-              <b>
-                <a href="">Edit</a>
-                <a href="">Del</a>
-              </b>
-            </span>
-          </li> -->
-          <li v-for="(todo, index) in todos" :key="index">
+          <li v-for="(todo, i) in todos" :key="i">
             <i
               :class="['fa-check-square', todo.state === 'yet' ? 'far' : 'fas']"
-              @click="checkItem(index)"
+              @click="checkItem(i)"
             ></i>
             <span>
               {{ todo.text }}
               <b>
-                <a href="">Edit</a>
-                <a href="">Del</a>
+                <a href="" @click.prevent="editShow(i)">Edit</a>
+                <a href="" @click.prevent="deleteTodo(i)">Del</a>
               </b>
             </span>
           </li>
@@ -47,12 +39,15 @@ export default {
   data() {
     return {
       addItemText: '',
+      crrEditItem: -1,
+      writeState: 'add',
+      editItemText: '',
       todos: [
         { text: "공부하기", state: "yet" },
         { text: "운동하기", state: "done" },
         { text: "글쓰기", state: "done" },
       ],
-    };
+    }
   },
   methods: {
     addItem() {
@@ -73,6 +68,19 @@ export default {
       } else {
         this.todos[index].state = 'yet';
       }
+    },
+    editShow(index1) {
+      console.log(`index >> ${index1}`);
+      this.crrEditItem = index1;
+      this.writeState = 'edit';
+      this.editItemText = this.todos[index1].text;
+    },
+    editSave() {
+      this.todos[this.crrEditItem].text = this.editItemText;
+      this.writeState = 'add';
+    },
+    deleteTodo(index1) {
+      this.todos.splice(index1, 1);
     }
   }, 
   mounted() {
