8117bf4368f959d921faad57383f2d7cc5077e69
jaehyuk <jaehyuk.pyon@gmail.com>
2023-02-10 11:22:43 +0900

    before using component

--- my-hacker-2/src/views/AskView.vue
+++ my-hacker-2/src/views/AskView.vue
@@ -1,13 +1,41 @@
 <template>
-  <div>AskView.vue</div>
+  <div>
+    AskView.vue <br><hr>
+    <ul  class="list">
+      <li v-for="(item, index) in list" :key="item.id">
+        <b>{{ item.time_ago }}</b>
+        <span>
+          by
+          <a href="">{{ item.user }}</a> </span
+        ><br />
+        <a href="#">{{ item.title }}</a>
+        <a href=""
+          >&nbsp;<i>({{ item.comments_count }})</i></a
+        >
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {
+import axios from "axios";
 
-}
+export default {
+  data() {
+    return {
+      list: [],
+    };
+  },
+  created() {
+    console.log("AskView.vue created.");
+    axios.get(`https://api.hnpwa.com/v0/ask/1.json`).then((response) => {
+      this.list = response.data;
+      console.log(this.list);
+    });
+  },
+};
 </script>
 
-<style>
+<style scoped>
 
 </style>
--- my-hacker-2/src/views/JobsView.vue
+++ my-hacker-2/src/views/JobsView.vue
@@ -1,13 +1,38 @@
 <template>
-  <div>JobsView.vue</div>
+  <div>
+    JobsView.vue <br><hr>
+    <ul  class="list">
+      <li v-for="(item, index) in list" :key="item.id">
+        <b>{{ item.time_ago }}</b>
+        <span>
+          by
+          <a href="">{{ item.user }}</a> </span
+        ><br />
+        <a href="#">{{ item.title }}</a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {
+import axios from "axios";
 
-}
+export default {
+  data() {
+    return {
+      list: [],
+    };
+  },
+  created() {
+    console.log("JobsView.vue created.");
+    axios.get(`https://api.hnpwa.com/v0/jobs/1.json`).then((response) => {
+      this.list = response.data;
+      console.log(this.list);
+    });
+  },
+};
 </script>
 
-<style>
+<style scoped>
 
 </style>
