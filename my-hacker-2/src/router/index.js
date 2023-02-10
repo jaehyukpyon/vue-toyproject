df1027d43f9aee80ce84995fb9072a9682c90778
jaehyuk <jaehyuk.pyon@gmail.com>
2023-02-10 09:46:25 +0900

    add router link(path) in index.js

--- my-hacker-2/src/router/index.js
+++ my-hacker-2/src/router/index.js
@@ -1,20 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+import NewsView from '../views/NewsView.vue'
+import AskView from '../views/AskView.vue'
+import JobsView from '../views/JobsView.vue'
+
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
   },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // }
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/news',
+    name: 'news',
+    component: NewsView
+  },
+  {
+    path: '/ask',
+    name: 'ask',
+    component: AskView
+  },
+  {
+    path: '/jobs',
+    name: 'jobs',
+    component: JobsView
+  },
 ]
 
 const router = createRouter({
---
+++ my-hacker-2/src/views/AskView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>AskView.vue</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
